baf2c37e1d4393fa8673f3eb077af38d5a23d92c
nirmalKumawat-kgp <nirmal.kumawat@kgpian.iitkgp.ac.in>
2022-04-22 14:00:03 +0530

    Hosting Changes

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://sreevidhya.herokuapp.com/api/"
+      ? "https://sreevidhyaa.herokuapp.com/api/"
       : "http://localhost:3006/api/",
 });
 export default API;
